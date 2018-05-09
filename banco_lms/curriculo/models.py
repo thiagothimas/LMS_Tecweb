4b4012f60cdb75efbdafc01afc300ca12c80cefb
Kallyne Barbosa <36283393+Kallyne@users.noreply.github.com>
2018-05-09 19:28:07 -0300

    Update models.py

--- banco_lms/curriculo/models.py
+++ banco_lms/curriculo/models.py
@@ -9,17 +9,17 @@ class Curso(models.Model):
 		
 class Disciplina(models.Model):
     
-    nome = models.CharField("nome", max_length=60)
+    nome = models.CharField("nome", max_length=100)
     data_expiracao = models.DateField("data_expiracao")
-    status = models.CharField("status", max_length=10)
-    plano_ensino = models.CharField("plano_ensino",max_length=3000)
+    status = models.CharField("status", max_length=100)
+    plano_ensino = models.CharField("plano_ensino",max_length=5000)
     carga_horaria = models.IntegerField("carga_horaria")
-    competencias = models.CharField("competencias",max_length=3000)
-    habilidade = models.CharField("habilidade",max_length=3000)
-    ementa = models.CharField("ementa",max_length=3000)
-    conteudo_programatico = models.CharField("conteudo_programatico",max_length=3000)
-    bibliografia_basica = models.CharField("bibliografia_basica",max_length=3000)
-    bibliografia_complementar = models.CharField("bibliografia_complementar",max_length=3000)
+    competencias = models.CharField("competencias",max_length=5000)
+    habilidade = models.CharField("habilidade",max_length=5000)
+    ementa = models.CharField("ementa",max_length=5000)
+    conteudo_programatico = models.CharField("conteudo_programatico",max_length=5000)
+    bibliografia_basica = models.CharField("bibliografia_basica",max_length=5000)
+    bibliografia_complementar = models.CharField("bibliografia_complementar",max_length=5000)
     percentual_pratico = models.IntegerField("percentual_pratico")
     percentual_teorico = models.IntegerField("percentual_teorico")
 
@@ -33,9 +33,9 @@ class Disciplina_Ofertada(models.Model):
     ano = models.IntegerField("ano")
     semestre = models.CharField ("semestre",max_length=1)
     turma = models.CharField ("turma",max_length=2)
-    metodologia = models.CharField("metodologia",max_length=3000)
-    recursos = models.CharField("recursos",max_length=3000)
-    criterio_avaliacao = models.CharField("criterio_avaliacao",max_length=3000)
+    metodologia = models.CharField("metodologia",max_length=5000)
+    recursos = models.CharField("recursos",max_length=5000)
+    criterio_avaliacao = models.CharField("criterio_avaliacao",max_length=5000)
     plano_aulas = models.CharField("plano_aulas",max_length=10000)
 
     def __str__(self):
